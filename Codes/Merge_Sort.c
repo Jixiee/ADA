424fb301ba9f8a5f127e2a09d47b470f4e093652
Jigyasa Kumari <137871881+Jixiee@users.noreply.github.com>
2023-08-31 21:51:53 +0530

    Create Merge_Sort.c

---
+++ Codes/Merge_Sort.c
@@ -0,0 +1,70 @@
+// Write a program to implement Merge sort in C
+#include <stdio.h>
+void merge(int arr[], int left, int middle, int right) {
+ int n1 = middle 
+- left + 1;
+ int n2 = right 
+- middle;
+ int L[n1], R[n2];
+ for (int i = 0; i < n1; i++)
+ L[i] = arr[left + i];
+ for (int j = 0; j < n2; j++)
+ R[j] = arr[middle + 1 + j];
+ int i = 0, j = 0, k = left;
+ while (i < n1 && j < n2) {
+ if (L[i] <= R[j]) {
+ arr[k] = L[i];
+ i++;
+ } else {
+ arr[k] = R[j];
+ j++;
+ 
+}
+ k++;
+ }
+ while (i < n1) {
+ arr[k] = L[i];
+ i++;
+ k++;
+ }
+ while (j < n2) {
+ arr[k] = R[j];
+ j++;
+ k++;
+ }
+}
+void mergeSort(int arr[], int left, int right) {
+ if (left < right) {
+ int middle = left + (right - left) / 2;
+ mergeSort(arr, left, middle);
+ mergeSort(arr, middle + 1, right);
+ merge(arr, left, middle, right);
+ }
+}
+int main() {
+ int n;
+ printf("=====================================");
+ printf("\n <Your Name>");
+ printf("\n <Enrollment Number>");
+ printf("\n=====================================");
+ printf("\nEnter the number of elements: ");
+ scanf("%d", &n);
+ int arr[n];
+ printf("Enter the elements:\n");
+ for (int i = 0; i < n; i++) {
+ scanf("%d", &arr[i]);
+ }
+ printf("\n=====================================");
+ printf("\nUnsorted array: ");
+ for (int i = 0; i < n; i++) {
+ printf("%d ", arr[i]);
+ }
+ mergeSort(arr, 0, n - 1);
+ printf("\nSorted array: ");
+ for (int i = 0; i < n; i++) {
+ printf("%d ", arr[i]);
+ }
+ printf("\n");
+ printf("=====================================");
+ return 0;
+}
