a17aae0e88b75038a878da1fd7a3769d989f04bd
Jigyasa Kumari <137871881+Jixiee@users.noreply.github.com>
2023-08-31 21:42:18 +0530

    Create Binary_Search_Recursive.c

---
+++ Codes/Binary_Search_Recursive.c
@@ -0,0 +1,44 @@
+// Write a program to implement Binary search using recursive method in C
+#include<stdio.h>
+#include<stdlib.h>
+#define size 10
+int binsearch(int[], int, int, int);
+int main() {
+printf("========================================");
+printf("\nName: <Your Name> \n");
+printf("En No. = <Enrollment Number> \n");
+printf("========================================");
+int num, i, key, position;
+int low, high, list[size];
+printf("\nEnter the total number of elements: ");
+scanf("%d", &num);
+printf("\nEnter the elements of list :");
+for (i = 0; i < num; i++) {
+scanf("%d", &list[i]);
+}
+low = 0;
+high = num - 1;
+printf("\n========================================");
+printf("\nEnter element to be searched : ");
+scanf("%d", &key);
+position = binsearch(list, key, low, high);
+if (position != -1) {
+printf("\nNumber present at %d", (position + 1));
+} else
+printf("\n The number is not present in the list");
+printf("\n========================================");
+return (0);
+}
+int binsearch(int a[], int x, int low, int high) {
+int mid;
+if (low > high)
+return -1;
+mid = (low + high) / 2;
+if (x == a[mid]) {
+return (mid);
+} else if (x < a[mid]) {
+binsearch(a, x, low, mid - 1);
+} else {
+binsearch(a, x, mid + 1, high);
+}
+}
