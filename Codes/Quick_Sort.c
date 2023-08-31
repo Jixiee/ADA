01763cc6d05b19f463fe575dba51e27ab4e5a06a
Jigyasa Kumari <137871881+Jixiee@users.noreply.github.com>
2023-08-31 21:49:50 +0530

    Create Quick_Sort.c

---
+++ Codes/Quick_Sort.c
@@ -0,0 +1,58 @@
+// Write a program to implement Quick Sort in C
+#include <stdio.h>
+void swap(int* a, int* b) {
+ int temp = *a;
+ *a = *b;
+ *b = temp; }
+int partition(int arr[], int low, int high) {
+ int pivot = arr[high];
+ int i = (low 
+- 1);
+ for (int j = low; j <= high 
+- 1; j++) {
+ if (arr[j] < pivot) {
+ i++;
+ swap(&arr[i], &arr[j]);
+ 
+}
+ 
+}
+ swap(&arr[i + 1], &arr[high]);
+ return (i + 1); }
+void quick_sort(int arr[], int low, int high) {
+ if (low < high) {
+ int pi = partition(arr, low, high);
+ quick_sort(arr, low, pi 
+- 1);
+ quick_sort(arr, pi + 1, high);
+ 
+}
+}
+int main() {
+ int n;
+ printf("=====================================");
+ printf("\n <Your Name>");
+ printf("\n <Enrollment Number>");
+ printf("\n=====================================");
+ printf("\nEnter the number of elements: ");
+ scanf("%d", &n);
+ int arr[n];
+ printf("Enter %d elements:", n);
+ for (int i = 0; i < n; i++) {
+ scanf("%d", &arr[i]);
+ }
+ printf("\n=====================================");
+ printf("\nUnsorted array: ");
+ for (int i = 0; i < n; i++) {
+ printf("%d ", arr[i]);
+ }
+ printf("\n");
+ quick_sort(arr, 0, n - 1);
+ printf("Sorted array: ");
+ for (int i = 0; i < n; i++) {
+ printf("%d ", arr[i]);
+ }
+ printf("\n");
+ printf("=====================================");
+ return 0;
+}
